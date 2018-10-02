6a09a0fc8a73a7b0f0f1a24009be84bc5ed61292
m31271n <m31271n@pm.me>
2018-10-02 19:43:23 +0800

    refactor: adjust interface of director.init

--- src/managers/director.js
+++ src/managers/director.js
@@ -3,11 +3,10 @@ import EventEmitter from 'eventemitter3'
 import ViewAdapter from '../components/ViewAdapter'
 
 class Director extends EventEmitter {
-  init(container, params) {
-    this.container = container
-    const view = container.querySelector('canvas')
-    const _params = Object.assign({ view }, params)
-    const app = new PIXI.Application(_params)
+  init(selector, options) {
+    this.container = document.querySelector(selector)
+    const app = new PIXI.Application(options)
+    this.container.appendChild(app.view)
     this.app = app
     this.scenes = {}
     app.stage.isRoot = true
@@ -38,16 +37,19 @@ class Director extends EventEmitter {
     }
   }
 
-   updateView() {
-     const { app } = this
-     //const { innerWidth, innerHeight, devicePixelRatio = 1 } = window
-     const { devicePixelRatio = 1 } = window
-     // innerWidth 在设备旋转后，值不正确，clientWidth会剔除掉scrollbar的宽度
-     const { clientWidth: innerWidth, clientHeight: innerHeight } = document.documentElement
-     app.width = app.view.width = innerWidth * devicePixelRatio
-     app.height = app.view.height = innerHeight * devicePixelRatio
-     app.renderer.resize(app.width, app.height)
-     this.devicePixelRatio = devicePixelRatio
+  updateView() {
+    const { app } = this
+    //const { innerWidth, innerHeight, devicePixelRatio = 1 } = window
+    const { devicePixelRatio = 1 } = window
+    // innerWidth 在设备旋转后，值不正确，clientWidth会剔除掉scrollbar的宽度
+    const {
+      clientWidth: innerWidth,
+      clientHeight: innerHeight,
+    } = document.documentElement
+    app.width = app.view.width = innerWidth * devicePixelRatio
+    app.height = app.view.height = innerHeight * devicePixelRatio
+    app.renderer.resize(app.width, app.height)
+    this.devicePixelRatio = devicePixelRatio
   }
 
   handleResize = () => {
