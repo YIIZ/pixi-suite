011587b694825bc5dff78b54276a1623084af76f
relaxgo <patientzhe@gmail.com>
2018-10-05 13:01:04 +0800

    fix button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,37 @@
 import Base from './Base'
+import StatusSwitch from './StatusSwitch'
 
 export default class Button extends Base {
   onEnable() {
     this.node.interactive = true
-    this.node.on('tap', this.handleClick, this)
+    this.node.on('touchstart', this.handleDown, this)
   }
 
   onDisable() {
-    this.node.off('tap', this.handleClick, this)
+    this.node.off('touchstart', this.handleDown, this)
+  }
+
+  handleDown() {
+    const ss = this.node.getComponent(StatusSwitch)
+    if (ss) ss.switch('on')
+
+    this.node.on('touchcancel', this.handleCancel, this)
+    this.node.on('touchendoutside', this.handleCancel, this)
+    this.node.on('touchend', this.handleClick, this)
+  }
+
+  handleCancel(evt) {
+    const ss = this.node.getComponent(StatusSwitch)
+    if (ss) ss.switch('off')
+
+    this.node.off('touchcancel', this.handleCancel, this)
+    this.node.off('touchendoutside', this.handleCancel, this)
+    this.node.off('touchend', this.handleClick, this)
   }
 
   handleClick(evt) {
+    this.handleCancel(evt)
     if (this.node.onClick) this.node.onClick(evt)
   }
-
 }
 
--- src/components/StatusSwitch.js
+++ src/components/StatusSwitch.js
@@ -4,7 +4,7 @@ export default class StatusSwitch extends Base {
   onEnable() {
     this.off = this.node.children.find(c => c.status === 'off')
     this.on = this.node.children.find(c => c.status === 'on')
-    this.status = 'off'
+    this.switch('off')
   }
 
   switch(status) {
