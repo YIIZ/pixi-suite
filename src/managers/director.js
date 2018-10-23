57acdb838682c1dfc11c7f6bf833a851527cdfb8
Bin Xin <rhyzix@gmail.com>
2018-10-23 15:00:01 +0800

    custom devicePixelRatio

--- src/managers/director.js
+++ src/managers/director.js
@@ -3,13 +3,14 @@ import EventEmitter from 'eventemitter3'
 import ViewAdapter from '../components/ViewAdapter'
 
 class Director extends EventEmitter {
-  init(container, params) {
+  init(container, params, devicePixelRatio=(window.devicePixelRatio || 1)) {
     this.container = container
     const view = container.querySelector('canvas')
     const _params = Object.assign({ view }, params)
     const app = new PIXI.Application(_params)
     this.app = app
     this.scenes = {}
+    this.devicePixelRatio = devicePixelRatio
     app.stage.isRoot = true
 
     this.updateView()
@@ -51,13 +52,12 @@ class Director extends EventEmitter {
    updateView() {
      const { app } = this
      //const { innerWidth, innerHeight, devicePixelRatio = 1 } = window
-     const { devicePixelRatio = 1 } = window
+     const { devicePixelRatio } = this
      // innerWidth 在设备旋转后，值不正确，clientWidth会剔除掉scrollbar的宽度
      const { clientWidth: innerWidth, clientHeight: innerHeight } = document.documentElement
      app.width = app.view.width = innerWidth * devicePixelRatio
      app.height = app.view.height = innerHeight * devicePixelRatio
      app.renderer.resize(app.width, app.height)
-     this.devicePixelRatio = devicePixelRatio
   }
 
   handleResize = () => {
