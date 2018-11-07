917ee2b4b16084f2d710e7055e5c8ca9fc74bde5
relaxgo <patientzhe@gmail.com>
2018-11-07 11:28:31 +0800

    crop rotate

--- src/components/Crop.js
+++ src/components/Crop.js
@@ -3,6 +3,9 @@ import Base from './Base'
 import * as v2 from '../utils/v2'
 import director from '../managers/director'
 
+const a90 = Math.PI * 0.5
+const a45 = Math.PI * 0.25
+
 export default class Crop extends Base {
   target = this.node
   minScale = 0.1
@@ -11,6 +14,7 @@ export default class Crop extends Base {
     this.viewportRatio = 1 / director.app.stage.scale.x * director.devicePixelRatio
     this.node.interactive = true
     this.node.on('touchstart', this.handleTouchStart, this)
+    window.rr = this
   }
 
   onDisable() {
@@ -27,10 +31,13 @@ export default class Crop extends Base {
 
     this.startPosition = this.target.position.clone()
     this.startGravity = isMulti ? v2.gravity(p0, p1) : p0
-    this.startScale = this.target.scale.x
 
     if (isMulti) {
+      this.startScale = this.target.scale.x
       this.startDis = v2.distance(p0, p1)
+
+      this.startRotation = this.target.rotation
+      this.startVec = v2.subtract(p0, p1)
     }
 
     this.node.on('touchmove', this.handleTouchMove, this)
@@ -42,6 +49,7 @@ export default class Crop extends Base {
   handleTouchMove(evt) {
     this.handleMove(evt)
     this.handleScale(evt)
+    this.handleRotate(evt)
   }
 
   handleMove(evt) {
@@ -83,7 +91,35 @@ export default class Crop extends Base {
     this.target.scale.set(scale, scale)
   }
 
+  handleRotate(evt) {
+    const { touches } = evt.data.originalEvent
+    const isMulti = touches.length > 1
+    if (!isMulti) return
+
+    const [t0, t1] = touches
+
+    const p0 = { x: t0.pageX, y: t0.pageY }
+    const p1 = { x: t1.pageX, y: t1.pageY }
+    const vec = v2.subtract(p0, p1)
+    const angle = v2.singleAngle(this.startVec, vec)
+    this.target.rotation = this.startRotation + angle
+
+    this.target.rotation = a90
+  }
+
+  handleRotateEnd(evt) {
+    // TODO limit
+    const a = this.target.rotation % a90
+    if (a > a45) {
+      this.target.rotation += (a90 - a45)
+    } else {
+      this.target.rotation -= a
+    }
+  }
+
   handleTouchEnd(evt) {
+    this.handleRotateEnd(evt)
+
     this.node.off('touchmove', this.handleTouchMove, this)
     this.node.off('touchend', this.handleTouchEnd, this)
     this.node.off('touchcancel', this.handleTouchEnd, this)
