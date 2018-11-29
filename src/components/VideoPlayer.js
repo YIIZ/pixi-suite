5cd92ad1cdfc4d1c05f16ab5d9f54b9288b9eafc
relaxgo <patientzhe@gmail.com>
2018-11-29 13:10:12 +0800

    refactor video preload

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -4,35 +4,13 @@ import director from '../managers/director'
 import { updateDOMTransform } from '../utils/dom'
 
 
-const video = document.createElement('video')
-video.className = 'IIV'
-video.style.position = 'absolute'
-video.style.top = '0'
-video.style.left = '0'
-video.style.width = '1px'
-video.style.height = '1px'
-video.className = 'video'
-video.setAttribute('preload', 'auto')
-video.setAttribute('webkit-playsinline', '')
-video.setAttribute('playsinline', '')
-
-const unlock = () => {
-  const { paused } = video
-  video.play()
-  if (paused) {
-    video.pause()
-  }
-  window.removeEventListener('touchend', unlock)
-}
-window.addEventListener('touchend', unlock)
-
-
 export default class VideoPlayer extends Base {
   src = ''
   autoPlay = true
 
   static preload(src) {
-    video.src = src
+    this.initElement()
+    this.video.src = src
   }
 
   onEnable() {
@@ -57,11 +35,27 @@ export default class VideoPlayer extends Base {
   }
 
   initElement() {
-    // Stupid tencent x5 adaptation
-    //video.setAttribute('x5-playsinline', '')
-    //video.setAttribute('x5-video-player-type', 'h5')
-    //const orientation = cc.winSize.width > cc.winSize.height ? 'landscape' : 'portrait'
-    //video.setAttribute('x5-video-orientation', orientation)
+    if (this.video) return
+    const video = document.createElement('video')
+    video.className = 'IIV'
+    video.style.position = 'absolute'
+    video.style.top = '0'
+    video.style.left = '0'
+    video.style.width = '1px'
+    video.style.height = '1px'
+    video.className = 'video'
+    video.setAttribute('preload', 'auto')
+    video.setAttribute('webkit-playsinline', '')
+    video.setAttribute('playsinline', '')
+
+    const unlock = () => {
+      const { paused } = video
+      video.play()
+      if (paused) video.pause()
+      window.removeEventListener('touchend', unlock)
+    }
+    window.addEventListener('touchend', unlock)
+
     this.video = video
     director.container.appendChild(video)
   }
