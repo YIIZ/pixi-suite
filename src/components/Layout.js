41b4e2024dd7959aaf912727838660861afe0f8a
relaxgo <patientzhe@gmail.com>
2019-01-25 12:07:29 +0800

    layout support align
    need fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,6 +5,7 @@ export default class Layout extends Base {
   right = 0
   bottom = 0
   left = 0
+  align = 'left'
 
   spaceX = 15
   spaceY = 15
@@ -24,20 +25,43 @@ export default class Layout extends Base {
     const children = this.node.children
     const { spaceX, spaceY, top, right, bottom, left, width: w, height: h } = this
     const cursor = new PIXI.Point(left, top)
-    let vCursor = null
-    //let hCursor = null
-    children.forEach(c => {
+    let yCursor = top
+    let xCursor = left
+    let newLineIndex = 0
+    const breaks = []
+    children.forEach((c, index) => {
       c.x = cursor.x
       c.y = cursor.y
 
       const { width, height } = c
-      if (!vCursor) vCursor = new PIXI.Point(cursor.x, cursor.y + height + spaceY)
+      if (c.y + height > yCursor) {
+        yCursor = c.y + height
+      }
 
       cursor.x += width + spaceX
-      if (cursor.x + width > w) {
-        cursor.set(vCursor.x, vCursor.y)
-        vCursor.y += height + spaceY
+
+      xCursor = c.x + width
+      if (xCursor > w || index === children.length - 1) {
+        cursor.set(left, yCursor + spaceY)
+        breaks.push({ from: newLineIndex, to: index + 1, left, right: xCursor })
+        newLineIndex = index + 1
       }
+    })
+
+    this.updateAlign(breaks)
+  }
+
+  // FIXME child里如果有layout的话，需要child layout update完再次update
+  updateAlign(breaks) {
+    const { align, node: { children } } = this
+    if (align === 'left') return
+
+    const maxRight = breaks.reduce((m, v) => v.right > m ? v.right : m, 0)
+
+    breaks.forEach(b => {
+      const { from, to, left, right } = b
+      const offset = align === 'center' ? (maxRight - right) / 2 : maxRight - right
+      children.slice(from, to).forEach(item => item.x += offset)
     })
   }
 }
