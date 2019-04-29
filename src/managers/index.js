116528ea2fd43278c93630bf1c4f89c2f331b971
relaxgo <patientzhe@gmail.com>
2019-04-29 21:37:28 +0800

    rename toaster

--- src/managers/index.js
+++ src/managers/index.js
@@ -1,4 +1,4 @@
 export { default as director } from './director.js'
 export { default as modalManager } from './modalManager.js'
-export { default as toast } from './toast.js'
+export { default as toaster, default as toast } from './toaster.js'
 export { default as widgetManager } from './widgetManager.js'
