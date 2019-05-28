7721be8121b3e067b4748a05a871872707a9e2d8
relaxgo <patientzhe@gmail.com>
2019-05-28 10:02:25 +0800

    fix update dom

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -2,19 +2,17 @@ export const updateDOMTransform = (node, elem, devicePixelRatio = 2) => {
   node._recursivePostUpdateTransform()
 
   const wt = node.transform.worldTransform.clone()
-  wt.scale(1/devicePixelRatio, 1/devicePixelRatio)
-
-  // TODO 真正理解运用
+  // wt.scale(1/devicePixelRatio, 1/devicePixelRatio)
   // scale 会影响input的placeholder
-  const matrix = `matrix(${wt.a}, ${wt.b}, ${wt.c}, ${wt.d}, ${wt.tx}, ${wt.ty})`
+  const matrix = `matrix(${wt.a}, ${wt.b}, ${wt.c}, ${wt.d}, ${wt.tx/devicePixelRatio}, ${wt.ty/devicePixelRatio})`
   const bd = node.getLocalBounds()
 
   Object.assign(elem.style, {
     position: 'absolute',
     top: '0',
     left: '0',
-    width: `${bd.width}px`,
-    height: `${bd.height}px`,
+    width: `${bd.width/devicePixelRatio}px`,
+    height: `${bd.height/devicePixelRatio}px`,
     webkitTransformOrigin: '0 0 0',
     transformOrigin: '0 0 0',
     webkitTransform: matrix,
