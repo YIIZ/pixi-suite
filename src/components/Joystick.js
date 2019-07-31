c8c5b134fd7294c532ef1f498a2576bc2d05cfad
relaxgo <patientzhe@gmail.com>
2019-07-31 15:46:55 +0800

    update joystick

--- src/components/Joystick.js
+++ src/components/Joystick.js
@@ -1,4 +1,4 @@
-import { Point } from 'pixi.js'
+import { Point, UPDATE_PRIORITY } from 'pixi.js'
 import { tween } from 'popmotion'
 import director from '../managers/director'
 
@@ -17,7 +17,7 @@ export default class Joystick extends Base {
     this.pane.interactive = true
     // FIXME 范围的计算
     //this.r = this.pane.width / 2 - this.point.width / 4
-    this.r = this.pane.width / 2 - this.point.width / 2
+    this.r = this.pane.width / 2 - this.point.width
     this.pane.on('touchstart', this.handleDown, this)
   }
 
@@ -37,7 +37,7 @@ export default class Joystick extends Base {
     this.globalPos = new Point()
     this.pos = {}
 
-    director.ticker.add(this.postControl, this)
+    director.ticker.add(this.postControl, this, UPDATE_PRIORITY.INTERACTION)
     this.pane.on('touchmove', this.handleControl, this)
     this.pane.on('touchcancel', this.handleCancel, this)
     this.pane.on('touchendoutside', this.handleCancel, this)
@@ -49,7 +49,7 @@ export default class Joystick extends Base {
   handleControl(evt) {
     const touches = evt.data.originalEvent.touches
 
-    this.globalPos.copy(evt.data.global)
+    this.globalPos.copyFrom(evt.data.global)
     if (this.identifier !== evt.data.identifier) {
       const touch = touches.find(t => t.identifier === identifier)
       interaction.mapPositionToPoint(this.globalPos, touch.clientX, touch.clientY)
@@ -66,15 +66,29 @@ export default class Joystick extends Base {
       dis = r
     }
 
-    this.point.position.copy(pos)
+    this.updatePosition(pos)
     this.pos.x =  pos.x / r
     this.pos.y = pos.y / r
     this.pos.dis = dis / r
   }
 
   postControl() {
-    if (!this.isControling || !this.node.onControl || !this.pos) return
-    this.node.onControl(this.pos)
+    if (!this.isControling || !this.pos) return
+    if (this.node.onControl) {
+      this.node.onControl(this.pos)
+    } else {
+      const { x } = this.pos
+      if (x > 0) this.node.input.right = true
+      if (x < 0) this.node.input.left = true
+    }
+  }
+
+  updatePosition = (p) => {
+    if (this.node.direction === 'x') {
+      this.point.position.x = p.x
+    } else {
+      this.point.position.copyFrom(p)
+    }
   }
 
   handleCancel(evt) {
@@ -83,7 +97,7 @@ export default class Joystick extends Base {
     const from = { x: this.point.x, y: this.point.y }
     const to = { x: 0, y: 0 }
     this.backAction = tween({ from, to, duration: 400 })
-    .start(v => this.point.position.copy(v))
+    .start(this.updatePosition)
 
     director.ticker.remove(this.postControl, this)
     this.pane.off('touchmove', this.handleControl, this)
