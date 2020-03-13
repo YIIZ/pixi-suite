c5a1a0b9db1c54ae7b94f2d00ff93c357ee53ab0
relaxgo <patientzhe@gmail.com>
2020-03-13 13:47:25 +0800

    layout set pivot auto

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,3 @@
-import { Point } from 'pixi.js'
 import Base from './Base'
 
 const defaultLayout = {
@@ -7,6 +6,7 @@ const defaultLayout = {
   bottom: 0,
   left: 0,
   align: 'left',
+  pivot: { x: 0, y: 0 },
   spaceX: 15,
   spaceY: 15,
   width: 0,
@@ -16,10 +16,10 @@ const defaultLayout = {
 export default class Layout extends Base {
   onEnable() {
     if (!this.node.layout) {
-      this.node.layout = {...defaultLayout}
+      this.node.layout = { ...defaultLayout }
     } else {
       const { layout } = this.node
-      Object.keys(defaultLayout).map((k) => {
+      Object.keys(defaultLayout).map(k => {
         if (typeof layout[k] === 'undefined') {
           layout[k] = layout[k] || defaultLayout[k]
         }
@@ -36,7 +36,7 @@ export default class Layout extends Base {
     // TODO custom direction
     const children = this.node.children
     const { spaceX, spaceY, top, right, bottom, left, width: w, height: h } = this.node.layout
-    const cursor = new Point(left, top)
+    const cursor = { x: left, y: top }
     let yCursor = top
     let xCursor = left
     let newLineIndex = 0
@@ -54,26 +54,38 @@ export default class Layout extends Base {
 
       xCursor = c.x + width
       if (xCursor + spaceX >= w || index === children.length - 1) {
-        cursor.set(left, yCursor + spaceY)
+        ;(cursor.x = left), (cursor.y = yCursor + spaceY)
         breaks.push({ from: newLineIndex, to: index + 1, left, right: xCursor })
         newLineIndex = index + 1
       }
     })
 
     this.updateAlign(breaks)
+    this.updatePivot()
   }
 
   // FIXME child里如果有layout的话，需要child layout update完再次update
   updateAlign(breaks) {
-    const { node: { children, layout: { align } } } = this
+    const {
+      node: {
+        children,
+        layout: { align },
+      },
+    } = this
     if (align === 'left') return
 
-    const maxRight = breaks.reduce((m, v) => v.right > m ? v.right : m, 0)
+    const maxRight = breaks.reduce((m, v) => (v.right > m ? v.right : m), 0)
 
     breaks.forEach(b => {
       const { from, to, left, right } = b
       const offset = align === 'center' ? (maxRight - right) / 2 : maxRight - right
-      children.slice(from, to).forEach(item => item.x += offset)
+      children.slice(from, to).forEach(item => (item.x += offset))
     })
+  }
+
+  updatePivot() {
+    const { x, y } = this.node.layout.pivot
+    const { width, height } = this.node
+    this.node.pivot.set(x * width, y * height)
   }
 }
