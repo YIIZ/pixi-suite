43fa28942a8b880fc62a7deced7bb8c3ebfe96ef
relaxgo <patientzhe@gmail.com>
2020-10-23 09:42:52 +0800

    add animation util

---
+++ src/utils/animation.js
@@ -0,0 +1,29 @@
+import { tween, easing } from 'popmotion'
+
+export const fadeIn = (node, option) => {
+  node.alpha = 0
+  const action = tween({ from: 0, to: 1, duration: 400 }).start((v) => {
+    if (node._destroyed) return action.stop()
+    node.alpha = v
+  })
+}
+
+export const fadeOut = (node, option, handler) => {
+  const action = tween({ from: node.alpha, to: 0, duration: 300 }).start({
+    update: (v) => {
+      if (node._destroyed) return action.stop()
+      node.alpha = v
+    },
+    complete: () => {
+      if (node._destroyed) return
+      if (typeof handler === 'function') {
+        handler(node)
+      } else if (handler === 'remove') {
+        if (node.parent) node.parent.removeChild(node)
+      } else {
+        console.log('destroy')
+        node.destroy({ children: true })
+      }
+    },
+  })
+}
