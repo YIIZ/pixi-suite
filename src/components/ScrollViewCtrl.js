c16d52ebe80a7124230896a7e927f74df0f2fd9b
relaxgo <patientzhe@gmail.com>
2020-12-07 10:46:53 +0800

    scroll view add nomask prop

--- src/components/ScrollViewCtrl.js
+++ src/components/ScrollViewCtrl.js
@@ -11,16 +11,19 @@ export default class ScrollViewCtrl extends Base {
   freezing = false
 
   onEnable() {
-    if (!this.view) this.view = this.node.children.find(n => n.scrollPart === 'view')
-    if (!this.scroller) this.scroller = this.node.children.find(n => n.scrollPart === 'bar').getComponent(ScrollBarCtrl)
-    if (!this.content) this.content = this.view.children.find(n => n.scrollPart === 'content')
+    if (!this.view) this.view = this.node.children.find((n) => n.scrollPart === 'view')
+    if (!this.scroller)
+      this.scroller = this.node.children.find((n) => n.scrollPart === 'bar').getComponent(ScrollBarCtrl)
+    if (!this.content) this.content = this.view.children.find((n) => n.scrollPart === 'content')
 
     this.view.interactive = true
     this.view.on('touchstart', this.handleTouchStart, this)
 
     const m = new Sprite(WHITE)
-    this.view.mask = m
-    this.view.addChildAt(m, 0)
+    if (!this.node.nomask) {
+      this.view.mask = m
+      this.view.addChildAt(m, 0)
+    }
 
     const { viewSize } = this.node
     if (viewSize) this.setSize(viewSize.w, viewSize.h)
@@ -28,8 +31,10 @@ export default class ScrollViewCtrl extends Base {
 
   onDisable() {
     this.view.off('touchstart', this.handleTouchStart, this)
-    this.view.removeChild(this.view.mask)
-    this.view.mask = null
+    if (this.view.mask) {
+      this.view.removeChild(this.view.mask)
+      this.view.mask = null
+    }
   }
 
   get paddingX() {
@@ -47,15 +52,17 @@ export default class ScrollViewCtrl extends Base {
   setSize(w, h) {
     this.w = w
     this.h = h
-    this.view.mask.width = w
-    this.view.mask.height = h
+    if (this.view.mask) {
+      this.view.mask.width = w
+      this.view.mask.height = h
+    }
 
     // TODO only support one direction
     if (this.direction === 'vertical') {
       this.scroller.updateParams({
         len: this.content.height + this.paddingY,
         visibleLen: h,
-        setter: v => this.content.y = v,
+        setter: (v) => (this.content.y = v),
         getter: () => this.content.y,
       })
       this.scroller.node.x = w - this.scroller.node.width
@@ -64,7 +71,7 @@ export default class ScrollViewCtrl extends Base {
         // FIXME width 数值不对, 需要在加某些值
         len: this.content.width + this.paddingY,
         visibleLen: w,
-        setter: v => this.content.x = v,
+        setter: (v) => (this.content.x = v),
         getter: () => this.content.x,
       })
       this.scroller.node.y = h + this.scroller.node.width + 5
